dac4dc38ab946a5e4dadb03055b739d3a9ceb578
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-02-22 23:30:21 +0500

    gun

---
+++ Part-2/gun.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+system("color 0B");
+cout << " +--^----------,--------,-----,--------^-, "<<endl;
+cout << "  | |||||||||   '--------'     |          O "<<endl;
+cout << "  '+---------------------------^----------| "<<endl;
+cout << "    ',---------,---------,--------------' "<<endl;
+cout << "      / XXXXX /'|         /' 		      "<<endl;
+cout << "     / XXXXX /  |        /' 		       "<<endl;
+cout << "    / XXXXX /'---------' 		        "<<endl;
+cout << "   / XXXXX /				        "<<endl;
+cout << "  / XXXXX /				            "<<endl;
+cout << " (_______(				         "<<endl;
+};
