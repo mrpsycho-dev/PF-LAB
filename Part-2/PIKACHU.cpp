1a66c5ede8d69659e8d3500d360f484f77d583d7
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-02-22 23:39:33 +0500

    car

---
+++ Part-2/PIKACHU.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+system("color 20");
+cout <<" ';-.          ---'             "<<endl;
+cout <<"   '.'!_...._/'.-'' 		"<<endl;
+cout <<"     !         /      , 	"<<endl;
+cout <<"     /()    () !    .' '-._	"<<endl;
+cout <<"    |)   .    ()!  /   _.'	"<<endl;
+cout <<"     |   -'-      ,; '. < 	"<<endl;
+cout <<"     ;.__       ,;|   > | 	"<<endl;
+cout <<"    / ,      / ,  |.-'.-' 	"<<endl;
+cout <<"   (_/      (_/ .;|.<' 		"<<endl;
+cout <<"      !     ,      ;-' 		"<<endl;
+cout <<"       >    !     / 		"<<endl;
+cout <<"      (_,-''> .' 		"<<endl;
+cout <<"        (_,' "			;
+return 0;
+};
---
+++ Part-2/vehicle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+cout << "       .--------. \n";
+cout << " ____/_____|___ \\___ \n";
+cout << " O    _   - |   _   ,*\n";
+cout << " '--(_)-------(_)--' \n";
+return 0; 
+}
