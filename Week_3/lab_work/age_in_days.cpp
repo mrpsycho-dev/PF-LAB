f39b9b6835aa851710b0cebe1cbf2cddd036b5ed
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-02-26 20:25:07 +0500

    age in days

---
+++ Week_3/lab_work/age_in_days.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float age, ageDays;
+  
+  cout<< "Enter age in years: "; cin>> age;
+  
+  ageDays = age * 365;
+  cout << "Your age in Days is " << ageDays;
+  
+  
+  
+};
