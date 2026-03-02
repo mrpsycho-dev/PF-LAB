48d2a78389968ee933694d0a82cc6032ff3c4ebc
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-02 23:50:02 +0500

    fix

--- Week_3/home_tasks/among_us_game.cpp
+++ Week_3/home_tasks/among_us_game.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
   
-int impCount,playerCount,chancePercent;
+int impCount, playerCount, chancePercent;
 cout<<"Enter number of imposters: ";
 cin>>impCount;
 cout<<"Enter total players: ";
 cin>>playerCount;
-chancePercent=100*impCount/playerCount;
+chancePercent=(impCount/playerCount)*100;
 cout<<"Chance of being imposter: "<<chancePercent<<"%"<<endl;
 
 return 0;
