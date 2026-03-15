042bb9f0c9a4e54ac2344ffc0663c3ddd311c710
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-15 10:04:02 +0500

    challenge 1 loop

---
+++ Week_5/lab_tasks/challenge_1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+cout <<"Multiplication table of 24: \n";
+  for (int i = 1; i<=10; i++){
+    cout<< "24  x  "<< i<< " = "<< i*24<<endl;
+  }
+
+cout <<"Multiplication table of 50: \n";
+  for (int i = 1; i<=10; i++){
+    cout<< "50  x  "<< i<< " = "<< i*50<<endl;
+  }
+
+  cout <<"Multiplication table of 29: \n";
+  for (int i = 1; i<=10; i++){
+    cout<< "29  x  "<< i<< " = "<< i*29<<endl;
+  }
+
+}
