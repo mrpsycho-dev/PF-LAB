0d8d1a36cb77ec574074a5c45b6f54c95250fea0
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-17 01:29:05 +0500

    challenge 2 complete

--- Week_5/lab_tasks/nested_loop/challenge_2.cpp
+++ Week_5/lab_tasks/nested_loop/challenge_2.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 int main(){
   string user , pass;
-  string name, courseName; int age;
+  string name="", courseName=""; int age;
   
-  cout <<"UNIVERSITY MANAGEMENT SYSTEM";
+  cout <<"UNIVERSITY MANAGEMENT SYSTEM\n";
+  for (int i= 0 ; i<3; i++){
   cout<<"Enter Username: "; cin >> user;
   cout<<"Enter Password: "; cin >> pass;
   
-  for (int i= 0 ; i<3; i++){
     
     if (user == "admin" && pass == "1234"){
       cout<< "Login Successful";
       
+      while(true){
+        
       cout<< R"(
       1. Add Student
       2. View Student
       3. Add Course
-      4. Exit)";
+      4. Exit
+      )";
       int x;
       cin>>x;
       if(x==1){
@@ -27,7 +30,7 @@ int main(){
   
         
       }
-      if(x==2){
+     else if(x==2){
         if(name!=""){
           
           cout<< name<< endl<< age;
@@ -41,12 +44,13 @@ int main(){
         cout<<"Program ends";
         return 0;
       } else {cout<<"Invalid choice.";}
+    }
       
     }else {
       
       cout<< "Wrong username or password.";
     }
-    if (i==3 && (user!= "admin" || pass!= "1234" )){
+    if (i==2 && (user!= "admin" || pass!= "1234" )){
       cout<< "3 wrong login attempts. ";
 return 0 ;  
     }
