21869eb489224c44ae9da35a421832729609f4cb
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-24 16:34:22 +0500

    something

---
+++ Week_6/lab_tasks/task10cp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string anything, something = "something ";
+  
+  cout<< "Enter a string: "; cin>> anything;
+  
+  cout<< something << anything;
+  
+}
