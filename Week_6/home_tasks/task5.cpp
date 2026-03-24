afcabb380bfdf3b4bb3b249fb90ef56213ba6062
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-25 04:58:39 +0500

    debug

--- Week_6/home_tasks/task5.cpp
+++ Week_6/home_tasks/task5.cpp
@@ -2,27 +2,24 @@
 using namespace std;
 
 int main(){
-  int n; cin>> n;
+  int n; cin >> n;
   string arr[n];
   int count = 0;
   char letter;
-  cout<< "Enter student names: \n";
-  for (int i=0; i<5; i++){
-    cin>> arr[i];
-    cout<<"Enter letter to check: "; cin>>letter;
-    
-    for (int j=0; j<5; j++){
-      
-      if(arr[i][j]== letter){
-      count++;  
-      }
-      
-      
-      
+
+  cout << "Enter student names:\n";
+  for (int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+
+  cout << "Enter letter to check: ";
+  cin >> letter;
+
+  for (int i = 0; i < n; i++){
+    if(arr[i][0] == letter){
+      count++;
     }
   }
-  
-  
-  cout<<"Total names starting with "<< letter << count;
-  
+
+  cout << "Total names starting with " << letter << " = " << count;
 }
