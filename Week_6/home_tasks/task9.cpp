457411cfec7b151bf693f5c725fbc467897713c2
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-03-29 00:48:16 +0500

    document format

--- Week_6/home_tasks/task9.cpp
+++ Week_6/home_tasks/task9.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   int choice;
   int n = 0;
 
   string bookNames[100];
   int quantity[100];
 
-  while(true){
+  while (true)
+  {
     cout << R"(
 
  ----------- Library Menu -----------
@@ -20,68 +22,88 @@ int main(){
  -----------------------------------
  )";
 
-    cout << "Enter your choice: "; cin >> choice;
+    cout << "Enter your choice: ";
+    cin >> choice;
 
-    if(choice == 1){
+    if (choice == 1)
+    {
       int add;
-      cout << "How many books to add: "; cin >> add;
+      cout << "How many books to add: ";
+      cin >> add;
 
-      for(int i = 0; i < add; i++){
-        cout << "Enter name of book " << n+1 << ": "; cin >> bookNames[n];
-        cout << "Enter quantity of " << bookNames[n] << ": ";  cin >> quantity[n];        
+      for (int i = 0; i < add; i++)
+      {
+        cout << "Enter name of book " << n + 1 << ": ";
+        cin >> bookNames[n];
+        cout << "Enter quantity of " << bookNames[n] << ": ";
+        cin >> quantity[n];
         n++;
       }
     }
 
-    else if(choice == 2){
+    else if (choice == 2)
+    {
       cout << "\n----------- All Books -----------\n";
-      
-      for(int i = 0; i < n; i++){
+
+      for (int i = 0; i < n; i++)
+      {
         cout << bookNames[i] << " : " << quantity[i] << " copies" << endl;
       }
     }
-    
-    else if(choice == 3){
+
+    else if (choice == 3)
+    {
       string borrow;
       cout << "Enter book name to borrow: ";
       cin >> borrow;
 
       bool found = false;
-      
-      for(int i = 0; i < n; i++){
-        if(bookNames[i] == borrow){
+
+      for (int i = 0; i < n; i++)
+      {
+        if (bookNames[i] == borrow)
+        {
           found = true;
-          
-          if(quantity[i] > 0){
+
+          if (quantity[i] > 0)
+          {
             quantity[i]--;
             cout << "Book borrowed successfully!\n";
-          } else {
+          }
+          else
+          {
             cout << "Book out of stock!\n";
           }
         }
       }
-      
-      if(!found){
+
+      if (!found)
+      {
         cout << "Book not found!\n";
       }
     }
-    
-    else if(choice == 4){
+
+    else if (choice == 4)
+    {
       cout << "\n-------- Available Books --------\n";
 
-      for(int i = 0; i < n; i++){
-        if(quantity[i] > 0){
+      for (int i = 0; i < n; i++)
+      {
+        if (quantity[i] > 0)
+        {
           cout << bookNames[i] << " : " << quantity[i] << " copies" << endl;
         }
       }
     }
 
-    else if(choice == 5){
+    else if (choice == 5)
+    {
       cout << "Programs ends.";
       return 0;
     }
 
-    else{
+    else
+    {
       cout << "Invalid choice!\n";
     }
   }
