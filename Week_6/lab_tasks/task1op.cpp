9fd31809e915a0c96a601ebc8aaadfedea0da849
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-04-07 13:36:41 +0500

    Update task1op.cpp

--- Week_6/lab_tasks/task1op.cpp
+++ Week_6/lab_tasks/task1op.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  
+int main()
+{
+
   string word;
-  cout<<"Enter a word "; cin>> word;
-  
-  for (int i=0; word[i]!='\0'; i++){
-    
-    cout<<word[i]<<" is found at position "<< i<<endl;
-    
+  cout << "Enter a word ";
+  cin >> word;
+
+  for (int i = 0; word[i] != '\0'; i++)
+  {
+
+    cout << word[i] << " is found at position " << i << endl;
   }
-  
-  
-  
 }
