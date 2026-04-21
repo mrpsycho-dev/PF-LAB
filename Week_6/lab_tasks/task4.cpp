adb6a40afdf0a613e9636eb2dda294392e26c385
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-04-21 11:13:25 +0500

    reformat

--- Week_6/lab_tasks/task4.cpp
+++ Week_6/lab_tasks/task4.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
   int arr[5];
-  
- for(int i = 0 ; i<arr[5];i++){
-  cin>> arr[i];
- } 
-  cout<<arr[0]<<arr[5-1];
+
+  for (int i = 0; i < arr[5]; i++)
+  {
+    cin >> arr[i];
+  }
+  cout << arr[0] << arr[4];
 }
--- Week_6/lab_tasks/task8cp.cpp
+++ Week_6/lab_tasks/task8cp.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   int arr1[2];
   int n;
-  
-  for (int i=0; i<2;i++){
-    cin>>arr1[i];
+
+  for (int i = 0; i < 2; i++)
+  {
+    cin >> arr1[i];
   }
-  cout<<"Enter number of elements in 2nd array: "; cin>> n;
-  int arr2[n]={};
-  int finalArray[n+1];
-   for (int i=0; i<n;i++){
-    cin>>arr2[i];
-       // Why can't we use this simple line?
-       // finalArray[i+2]={arr1[0],arr2[i] , arr1[1]};
-       // finalArray[arr1[0],arr2[i] , arr1[1]];
-    
+  cout << "Enter number of elements in 2nd array: ";
+  cin >> n;
+  int arr2[n] = {};
+  int finalArray[n + 2];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr2[i];
+    // Why can't we use this simple line?
+    // finalArray[i+2]={arr1[0],arr2[i] , arr1[1]};
+    // finalArray[arr1[0],arr2[i] , arr1[1]];
   }
   finalArray[0] = arr1[0];
-  for(int i =1; i<=n;i++){
-    
-    finalArray[i] = arr2[i-1]; 
+  for (int i = 1; i <= n; i++)
+  {
+
+    finalArray[i] = arr2[i - 1];
   }
-  finalArray[n+1] = arr1[1];
-   
- cout << "Final Array: [";
- for (int i = 0; i < n + 2; i++){
-   cout << finalArray[i];
-   if(i!=n+1){cout<<", ";}
+  finalArray[n + 1] = arr1[1];
+
+  cout << "Final Array: [";
+  for (int i = 0; i < n + 2; i++)
+  {
+    cout << finalArray[i];
+    if (i != n + 1)
+    {
+      cout << ", ";
+    }
   }
   cout << "]";
 }
