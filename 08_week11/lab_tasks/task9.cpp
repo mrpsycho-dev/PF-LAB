fc714e20cbc66898d7e5ab02d3fbf0c6b3013972
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-04-28 17:43:21 +0500

    error fix

--- 08_week11/lab_tasks/task9.cpp
+++ 08_week11/lab_tasks/task9.cpp
@@ -5,7 +5,43 @@ int countIdenticalRows(int arr[][3], int size);
 
 int main()
 {
+  int size;
+  cout << "Enter row size: ";
+  cin >> size;
+
+  int arr[size][3];
+
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      cout << "Enter element [" << i << "][" << j << "]: ";
+      cin >> arr[i][j];
+    }
+  }
+  cout << countIdenticalRows(arr, size);
 }
 int countIdenticalRows(int arr[][3], int size)
 {
+  int count = 0;
+  bool identical = false;
+  for (int i = 0; i < size; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      for (int k = 0; k < 3; k++)
+      {
+        if (arr[i][j] == arr[i][k])
+        {
+          identical = true;
+          break;
+        }
+      }
+      if (identical)
+      {
+        count++;
+      }
+    }
+  }
+  return count;
 }
