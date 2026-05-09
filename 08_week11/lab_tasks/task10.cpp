1b325945bf82be933e78d28af4c7e017c7db9f92
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-05-09 11:04:02 +0500

    task 10 gravity game complete

--- 08_week11/lab_tasks/task10.cpp
+++ 08_week11/lab_tasks/task10.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void displayWorld(arr[5][5]);
+void displayWorld(char arr[5][5]);
 bool setGravityStatus(bool status);
-int timeTick(int time);
+void timeTick(int time, char arr[5][5]);
 
+bool gravity = false;
 int main()
 {
   char objects[5][5] = {
@@ -13,23 +14,48 @@ int main()
       {'-', '#', '-', '-', '-'},
       {'#', '-', '#', '-', '#'},
       {'#', '-', '-', '-', '-'}};
-  bool gravity = false;
 
-  // displayWorld();
-  // setGravityStatus(true);
-  // timeTick(3);
-  // displayWorld();
+  displayWorld(objects);
+  setGravityStatus(true);
+  timeTick(3, objects);
+  displayWorld(objects);
 }
 
-void displayWorld(arr[5][5])
+void displayWorld(char arr[5][5])
 {
+  cout << "M A T R I X: \n";
   for (int i = 0; i < 5; i++)
   {
     for (int j = 0; j < 5; j++)
     {
       cout << arr[i][j] << "\t";
     }
+    cout << endl;
   }
 }
-bool setGravityStatus(bool status) {}
-int timeTick(int time) {}
+bool setGravityStatus(bool status)
+{
+  gravity = status;
+}
+void timeTick(int time, char arr[5][5])
+{
+  if (!gravity)
+  {
+    return;
+  }
+
+  for (int i = 0; i < time; i++)
+  {
+    for (int j = 4; j >= 0; j--)
+    {
+      for (int k = 0; k < 5; k++)
+      {
+        if (arr[j][k] == '#' && arr[j + 1][k] == '-')
+        {
+          arr[j][k] = '-';
+          arr[j + 1][k] = '#';
+        }
+      }
+    }
+  }
+}
