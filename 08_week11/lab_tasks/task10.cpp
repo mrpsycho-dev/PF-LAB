7a6415811e133f90b5008485cdb54eb9112a7c70
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-05-09 13:04:41 +0500

    black hole functionality

--- 08_week11/lab_tasks/task10.cpp
+++ 08_week11/lab_tasks/task10.cpp
@@ -7,6 +7,7 @@ void timeTick(int time, char arr[5][5]);
 
 bool gravity = false;
 bool isBlackHole = true;
+
 int main()
 {
   char objects[5][5] = {
@@ -18,7 +19,7 @@ int main()
 
   displayWorld(objects);
   setGravityStatus(true);
-  // isBlackHole = true;
+  isBlackHole = true;
   timeTick(3, objects);
   displayWorld(objects);
 }
@@ -28,17 +29,20 @@ void displayWorld(char arr[5][5])
   cout << "Y O U R    W O R L D: \n";
   for (int i = 0; i < 5; i++)
   {
-    for (int j = 0; j < 5; j++)
+    for (int i = 0; i < 5; i++)
     {
-      cout << arr[i][j] << "\t";
+      cout << arr[i][i] << "\t";
     }
     cout << endl;
   }
 }
+
 bool setGravityStatus(bool status)
 {
   gravity = status;
+  return gravity;
 }
+
 void timeTick(int time, char arr[5][5])
 {
   if (!gravity)
@@ -46,30 +50,43 @@ void timeTick(int time, char arr[5][5])
     return;
   }
 
-  for (int i = 0; i < time; i++)
+  if (isBlackHole)
   {
-    for (int j = 4; j >= 0; j--)
+    // it is because after every 5 timeTicks, our world returns to its original state. So:
+    int shifts = time % 5;
+
+    for (int s = 0; s < shifts; s++)
     {
-      for (int k = 0; k < 5; k++)
+      // Save the last row
+      char lastRow[5];
+      for (int j = 0; j < 5; j++)
       {
-        // if (isBlackHole)
-        // {
-        //   if (j == 0)
-        //   {
-        //     arr[j][k] = arr[4][k];
-        //   }
-        //   else
-        //   {
-        //     arr[j][k] = arr[j - 1][k];
-        //   }
-        // }
-        // else
-        if (j <= 3 && arr[j][k] == '#' && arr[j + 1][k] == '-')
+        lastRow[j] = arr[4][j];
+      }
+
+      for (int i = 4; i >= 1; i--)
+        for (int j = 0; j < 5; j++)
         {
-          arr[j][k] = '-';
-          arr[j + 1][k] = '#';
+          arr[i][j] = arr[i - 1][j];
         }
+
+      // Place last row to top
+      for (int j = 0; j < 5; j++)
+      {
+        arr[0][j] = lastRow[j];
       }
     }
+  }
+
+  else
+  {
+    for (int i = 0; i < time; i++)
+      for (int i = 3; i >= 0; i--)
+        for (int j = 0; j < 5; j++)
+          if (arr[i][j] == '#' && arr[i + 1][j] == '-')
+          {
+            arr[i][j] = '-';
+            arr[i + 1][j] = '#';
+          }
   }
 }
