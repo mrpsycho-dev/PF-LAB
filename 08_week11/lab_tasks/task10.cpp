a80979a28ab32d5a342478aff42ac96487edcdf4
Muhammad Abdullah <muhammadabdullahnazir6@gmail.com>
2026-05-09 13:14:08 +0500

    Update task10.cpp

--- 08_week11/lab_tasks/task10.cpp
+++ 08_week11/lab_tasks/task10.cpp
@@ -58,6 +58,7 @@ void timeTick(int time, char arr[5][5])
       for (int j = 0; j < 5; j++)
         lastRow[j] = arr[4][j];
 
+      // Shift each row lower by 1
       for (int i = 4; i >= 1; i--)
         for (int j = 0; j < 5; j++)
           arr[i][j] = arr[i - 1][j];
